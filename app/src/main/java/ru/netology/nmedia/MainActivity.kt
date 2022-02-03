fbf0eea126515c810f927b1c3579b4e927d917ec
Igorista <minelab08@gmail.com>
2022-02-03 21:52:15 +0300

    to fix

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,13 +1,11 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.dto.Post
-import java.lang.Math.round
-
+import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,55 +13,31 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            published = "21 мая в 18:36",
-            likedByMe = false,
-            sheared = false,
-            viewed = false
-        )
-        with(binding) {
-            author.text = post.author
-            published.text = post.published
-            content.text = post.content
-            if (post.likedByMe) {
-                like?.setImageResource(R.drawable.ic_like_24)
-            }
-
-            root.setOnClickListener {
-                Log.d("stuff", "stuff")
-            }
-
-            avatar.setOnClickListener {
-                Log.d("stuff", "avatar")
-            }
-
-            likesCount?.text = post.likes.toString()
-            like?.setOnClickListener {
-                Log.d("stuff", "like")
-                post.likedByMe = !post.likedByMe
+        val viewModel: PostViewModel by viewModels()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
                 like.setImageResource(
                     if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
                 )
                 if (post.likedByMe) post.likes++ else post.likes--
                 likesCount?.text = counting(post.likes)
-            }
-            sheareCount?.text = post.shears.toString()
-            sheared?.setOnClickListener {
-                Log.d("stuff", "sheared")
-                post.sheared = !post.sheared
-                if (post.sheared) post.shears++
-                sheareCount?.text = counting(post.shears)
-            }
-            viewsCount?.text = post.views.toString()
-            viewed?.setOnClickListener {
-                Log.d("stuff", "viewed")
-                post.viewed = !post.viewed
+                if (post.shared) post.shares++
+                sheareCount?.text = counting(post.shares)
                 if (post.viewed) post.views++
                 viewsCount?.text = counting(post.views)
             }
+        }
+        binding.like.setOnClickListener {
+            viewModel.like()
+        }
+        binding.shared.setOnClickListener {
+            viewModel.share()
+        }
+        binding.viewed.setOnClickListener {
+            viewModel.view()
         }
     }
 }
---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,15 @@
+package ru.netology.nmedia.dto.ru.netology.nmedia.dto
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    var likes: Int,
+    var shares: Int,
+    var views : Int,
+    val likedByMe: Boolean,
+    val shared: Boolean,
+    val viewed: Boolean,
+)
+
