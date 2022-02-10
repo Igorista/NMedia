288796061975759e5958d8e004f6517fafc04e83
Igorista <minelab08@gmail.com>
2022-02-10 22:15:35 +0300

    CRUDE

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -7,4 +7,7 @@ interface PostRepository {
     fun getAll(): LiveData<List<Post>>
     fun likeById(id: Long)
     fun shareById(id:Long)
+    fun save(post: Post)
+    fun cancel(post: Post)
+    fun removeById(id: Long)
 }
---
+++ app/src/main/java/ru/netology/nmedia/util/AndroidUtils.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia.util
+
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+
+object AndroidUtils {
+    fun hideKeyboard(view: EditText) {
+        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
